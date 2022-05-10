5b10a15b99b8414a64cecfd30414274a336d6543
bidok <bidokfeed@gmail.com>
2022-05-10 14:57:36 +0300

    lab 6, fuzzy logic

---
+++ src/com/company/FuzzyLogic.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author : Vasyl Bidiak
+ * @created : 25.04.2022
+ * @className : FuzzyLogic
+ **/
+
+public class FuzzyLogic {
+	public static void main(String[] args) {
+
+		Map<Double, String> map = new HashMap<>();
+		double n = 80;
+		double s = 1.0/(1.0+Math.pow((n-30)/12, 12));
+		map.put(s, "slow");
+		double m = 1.0/(1.0+Math.pow((n-70)/3, 6));
+		map.put(m, "medium");
+		double f = 1.0/(1.0+Math.pow((n-100)/6, 10));
+		map.put(f, "fast");
+
+		double biggest = map.keySet().stream().mapToDouble(aDouble -> aDouble).max().orElse(0.0);
+		System.out.printf("current speed are %s%n", map.get(biggest));
+	}
+}
